ca243f659a60363afc57ce71b9320025a6bf704a
Jair Velazquez <velazquezespinozaeliasjair@gmail.com>
2020-11-17 20:07:11 -0600

    Category Class implementation
    Category and CategoryService

---
+++ app/src/main/java/Backend/Category.java
@@ -0,0 +1,39 @@
+package Backend;
+public class Category {
+    private int idCategory;
+    private String name;
+    private String type;
+    private String transactionType;
+
+    public int getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+}
---
+++ app/src/main/java/Backend/CategoryService.java
@@ -0,0 +1,20 @@
+package Backend;
+
+import java.util.ArrayList;
+
+public class CategoryService {
+
+    public Category getCategory(Category category){
+        return category;
+    }
+
+    public Category updateCategory(Category category){
+        return category;
+    }
+    public Category addCategory(Category category){
+        return category;
+    }
+    public Category deleteCategory(Category category){
+
+    }
+}
